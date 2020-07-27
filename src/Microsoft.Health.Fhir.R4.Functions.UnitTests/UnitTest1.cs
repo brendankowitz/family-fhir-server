52a257345cafcc6519f83f11663b13d293ec4879
Brendan Kowitz <bkowitz@microsoft.com>
2020-07-27 11:04:12 -0700

    Adds a test project

---
+++ src/Microsoft.Health.Fhir.R4.Functions.UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Xunit;
+
+namespace Microsoft.Health.Fhir.R4.Functions.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
